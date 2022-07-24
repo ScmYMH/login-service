d9ca850355b083403c7d396a3b26fb0be76929f8
Mino94 <mh.kim368@gmail.com>
2022-07-24 17:06:56 +0900

    login update

--- src/main/java/com/scm/login/LoginApplication.java
+++ src/main/java/com/scm/login/LoginApplication.java
@@ -2,7 +2,12 @@ package com.scm.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
+@EnableEurekaClient
 @SpringBootApplication
 public class LoginApplication {
 
