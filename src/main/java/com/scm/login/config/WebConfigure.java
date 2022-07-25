b9768670d5ba07d60b70ed96fd75283f13c903ce
Mino94 <mh.kim368@gmail.com>
2022-07-25 10:24:50 +0900

    webconfig port change

--- src/main/java/com/scm/login/config/WebConfigure.java
+++ src/main/java/com/scm/login/config/WebConfigure.java
@@ -9,7 +9,7 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://192.168.0.43:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
