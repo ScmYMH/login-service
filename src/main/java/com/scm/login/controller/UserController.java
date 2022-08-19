109379bc3f20f86066b1986253000f00d174f1c5
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-08-19 17:16:22 +0900

    login update

--- src/main/java/com/scm/login/controller/UserController.java
+++ src/main/java/com/scm/login/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.scm.login.controller;
 
+import com.scm.login.aspect.TokenRequired;
+import com.scm.login.config.SecurityService;
 import com.scm.login.dto.LoginDto;
 import com.scm.login.dto.RequestLoginDto;
 import com.scm.login.entity.UserEntity;
@@ -7,7 +9,10 @@ import com.scm.login.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -19,11 +24,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping
-    public String check(){
-        return "check";
-    }
-
+    @Autowired
+    SecurityService securityService;
     @GetMapping("/all")
     public List<UserEntity> getUserAll(){
         return userService.getUser();
@@ -32,5 +34,22 @@ public class UserController {
     @PostMapping("/login")
     public LoginDto login(@RequestBody RequestLoginDto requestLoginDto){
         return userService.getLogin(requestLoginDto);
+    }
+    @GetMapping("/token")
+    @TokenRequired
+    public String getToken(){
+        ServletRequestAttributes requestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        String tokenBearer = request.getHeader("Authorization");
+
+        String subject = securityService.getSubject(tokenBearer);
+        return subject;
+    }
+    @TokenRequired
+    @GetMapping("/check")
+    public Boolean check(){
+        return true;
     }
 }
