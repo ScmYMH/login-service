070377289869a9a642b96cf39036880f7a7b147c
Mino94 <mh.kim368@gmail.com>
2022-08-22 14:42:13 +0900

    ip update

--- src/main/java/com/scm/login/config/WebConfigure.java
+++ src/main/java/com/scm/login/config/WebConfigure.java
@@ -9,7 +9,7 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.43:3000")
+                .allowedOrigins("http://192.168.0.47:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
